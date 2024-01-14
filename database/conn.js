bf188e454e7090b5a733ca74d2b14d9e6001edb1
DDR23 <roboreserva.andre@gmail.com>
2024-01-14 13:41:07 -0300

    fix:{novo database}

--- database/conn.js
+++ database/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function conn(){
   try {
-    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@board-task---database.ew81dgp.mongodb.net/${process.env.NODE_ENV}`);
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@database-projects---dnc.aqfezbi.mongodb.net/${process.env.NODE_ENV}`);
     console.log('mongodb conectado com sucesso');
   } catch (error) {
     console.log(`erro: ${error}`);
