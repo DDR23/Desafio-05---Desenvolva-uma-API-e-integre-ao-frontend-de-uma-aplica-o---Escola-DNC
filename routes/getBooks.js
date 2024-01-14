1f557306ecb4919615575d917a62ca3d5bee58a8
DDR23 <roboreserva.andre@gmail.com>
2024-01-14 14:38:44 -0300

    feat:{tratação de erro ao buscar livros}

--- routes/getBooks.js
+++ routes/getBooks.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const booksModel = require('../models/books');
+const path = require('path')
 
 router.get('', async (req, res) => {
   try {
     const books = await booksModel.find({});
     res.status(200).json({ books });
   } catch (error) {
-    res.status(500).send(error)
+    return res.status(500).sendFile(path.join(__dirname, '..', 'public', 'error.html'));
   }
 });
 
